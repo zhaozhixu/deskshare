c412bece8b6b2852359426d15fba7fd9da5ba205
zhixu <zzx.2013@qq.com>
2017-08-17 11:33:19 +0800

    local test OK, remote test failed

--- lib/dgram.c
+++ lib/dgram.c
@@ -42,8 +42,8 @@ int make_dgram_server_socket(int portnum)
           return -1;
      }
 
+     printf("Bind successfully at %d.\n", portnum);
      freeaddrinfo(result);
-
      return sfd;
 }
 
@@ -92,12 +92,13 @@ int make_dgram_client_socket(char *servhost, int servport, int localport)
 
           if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
                break;
+          perror("connect");
 
           close(sfd);
      }
      if (rp == NULL) {
           fprintf(stderr,
-                  "make_dgram_client_socket: cannot make socket %d",
+                  "make_dgram_client_socket: cannot make socket %d\n",
                   sfd);
           return -1;
      }
@@ -105,4 +106,43 @@ int make_dgram_client_socket(char *servhost, int servport, int localport)
      freeaddrinfo(result);
 
      return sfd;
+}
+
+int make_addr(char *host, char *port, struct addrinfo *addr)
+{
+     int s, sfd;
+     struct addrinfo hints;
+     struct addrinfo *result, *rp;
+
+     memset(&hints, 0, sizeof(struct addrinfo));
+     hints.ai_family = AF_UNSPEC;
+     hints.ai_socktype = SOCK_DGRAM;
+     hints.ai_flags = AI_PASSIVE;
+     hints.ai_protocol = 0;
+     hints.ai_canonname = NULL;
+     hints.ai_addr = NULL;
+     hints.ai_next = NULL;
+
+     s = getaddrinfo(host, port, &hints, &result);
+     if (s != 0) {
+          fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+          return -1;
+     }
+
+     for (rp = result; rp != NULL; rp  = rp->ai_next) {
+          sfd = socket(rp->ai_family, rp->ai_socktype,
+                       rp->ai_protocol);
+          if (sfd == -1)
+               continue;
+
+          if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
+               break;
+     }
+     close(sfd);
+     if (rp == NULL)
+          return -1;
+     memmove(addr, rp, sizeof(struct addrinfo));
+     /* addr = result; */
+
+     return 0;
 }
